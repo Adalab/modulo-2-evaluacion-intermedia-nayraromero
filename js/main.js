7269b63b9454de0378aac9794ca3bebbe8bc0d7f
nayraromero <nayraromero@gmail.com>
2022-10-31 11:05:45 +0100

    formating code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,42 @@
-const select=document.querySelector('.js-select');
-const button=document.querySelector('.js-button');
-const battle=document.querySelector('.js-text');
-const section=document.querySelector('.js-section');
-const userPoints=document.querySelector('.js-user');
-const computerPoints=document.querySelector('.js-computer');
-
+const select = document.querySelector('.js-select');
+const button = document.querySelector('.js-button');
+const battle = document.querySelector('.js-text');
+const section = document.querySelector('.js-section');
+const userPoints = document.querySelector('.js-user');
+const computerPoints = document.querySelector('.js-computer');
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * 5);
+  return Math.ceil(Math.random() * 5);
 }
 
-function compareBattle(){
-    const selectValue=parseInt(select.value);
-    const randomNumber=getRandomNumber();
-    console.log(selectValue, randomNumber);
-    if (randomNumber>selectValue){
-        battle.innerHTML='Ha ganado el Ejército del Mal! Vuelve a Intentarlo';
-    }else if (randomNumber===selectValue){
-        battle.innerHTML='Empate';
-    }else{
-        battle.innerHTML='Ha ganado el Ejército del Bien! Enhorabuena';
-    }
-    // for(i=0; i<10; i++){
-    //     if (randomNumber>selectValue){
-    //         computerPoints.innerHTML= [i];
-    //     }else if (randomNumber===selectValue){
-    //         computerPoints.innerHTML= [i];
-    //         userPoints.innerHTML= [i];
-    //     }else{
-    //         userPoints.innerHTML= [i];
-    //     }
-    // }
-};
-function showPoints(){
-    section.classList.remove('hidden');
-};
+function compareBattle() {
+  const selectValue = parseInt(select.value);
+  const randomNumber = getRandomNumber();
+  console.log(selectValue, randomNumber);
+  if (randomNumber > selectValue) {
+    battle.innerHTML = 'Ha ganado el Ejército del Mal! Vuelve a Intentarlo';
+  } else if (randomNumber === selectValue) {
+    battle.innerHTML = 'Empate';
+  } else {
+    battle.innerHTML = 'Ha ganado el Ejército del Bien! Enhorabuena';
+  }
+  // for(i=0; i<10; i++){
+  //     if (randomNumber>selectValue){
+  //         computerPoints.innerHTML= [i];
+  //     }else if (randomNumber===selectValue){
+  //         computerPoints.innerHTML= [i];
+  //         userPoints.innerHTML= [i];
+  //     }else{
+  //         userPoints.innerHTML= [i];
+  //     }
+  // }
+}
+function showPoints() {
+  section.classList.remove('hidden');
+}
 
-
-button.addEventListener('click', (event) =>{
-    event.preventDefault();
-    compareBattle();
-    showPoints();
-   
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  compareBattle();
+  showPoints();
 });
