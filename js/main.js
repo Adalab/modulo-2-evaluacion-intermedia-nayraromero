495df9894f46a35eee6bdd0d7cacb66755c2c136
nayraromero <nayraromero@gmail.com>
2022-11-09 20:26:00 +0100

    Define the logic to transform the random number into the computer selection AND  Add the functionality to select the strength of the evil race

--- js/main.js
+++ js/main.js
@@ -4,14 +4,30 @@
 
 const select = document.querySelector('.js-select');
 const button = document.querySelector('.js-button');
-const battle = document.querySelector('.js-text');
-const section = document.querySelector('.js-section');
-const userPoints = document.querySelector('.js-user');
-const computerPoints = document.querySelector('.js-computer');
+const result = document.querySelector('.js-text');
+// const section = document.querySelector('.js-section');
+// const userPoints = document.querySelector('.js-user');
+// const computerPoints = document.querySelector('.js-computer');
 
 //FUNCIONES
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
+}
+
+function getComputerRace() {
+  const randomNumber = getRandomNumber(5);
+  console.log('random number es ' + randomNumber);
+
+  let computerPower = 0;
+  if (randomNumber === 1 || randomNumber === 2 || randomNumber === 3) {
+    computerPower = 2;
+  } else if (randomNumber === 4) {
+    computerPower = 3;
+  } else if (randomNumber === 5) {
+    computerPower = 5;
+  }
+  console.log('la fuerza del ordenador es ' + computerPower);
+  return computerPower;
 }
 
 function compareBattle() {
@@ -41,6 +57,7 @@ function showPoints() {
 }
 function handleclick(event) {
   event.preventDefault();
+  const computerRace = getComputerRace();
   compareBattle();
   showPoints();
 }
